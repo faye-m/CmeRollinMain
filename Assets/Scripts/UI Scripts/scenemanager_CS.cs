d45dbdb826875db386459e20435601503e37e341
Faye <mendoza.fatimah@gmail.com>
2021-03-02 14:58:36 +1300

    Shader, UI Layout, and Scene Management Update
    Made a basic unlit shader and connected them to the base material labeled for the planned assets. Main menu layout has been started, and empty scenes were made for testing the load level management later.

---
+++ Assets/Scripts/UI Scripts/scenemanager_CS.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class scenemanager_CS : MonoBehaviour
+{
+    private string levelName;
+
+    public void LoadLevel(int stageNumber, int levelNumber) 
+    {
+        levelName = "stage" + stageNumber.ToString() + "_level" + levelNumber.ToString();
+
+        SceneManager.LoadScene(levelName, LoadSceneMode.Single);    
+    }
+
+    public void ReloadScene() 
+    {
+        Scene activeLevel = SceneManager.GetActiveScene();
+        levelName = activeLevel.name;
+        SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+    }
+}
